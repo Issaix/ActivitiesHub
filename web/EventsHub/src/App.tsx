3045f754659f373d6d8f5be05392b26209145799
Issai Brandon Aparicio Gutiérrez <agar.gamingyt400@gmail.com>
2026-09-15 20:15:37 -0600

    feat(parcial02) - update react path and add CORS

--- web/EventsHub/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <h3>
-      Events Hub
-    </h3>
-  )
-}
-
-export default App
---
+++ web/src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/v1/events')
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+  }, []);
+
+  return (
+    <div>
+      <h3 style={{ color: 'red'}}>Events Hub</h3>
+      <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App

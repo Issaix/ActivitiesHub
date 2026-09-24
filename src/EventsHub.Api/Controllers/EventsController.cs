4c50c7a467fc84a4220242b966a92d1bbe41e663
Issai Brandon Aparicio Gutiérrez <agar.gamingyt400@gmail.com>
2026-09-24 11:39:38 -0600

    feat(parcial02) - add create event

--- src/EventsHub.Api/Controllers/EventsController.cs
+++ src/EventsHub.Api/Controllers/EventsController.cs
@@ -1,3 +1,4 @@
+using EventsHub.Application.Events.Commands;
 using EventsHub.Application.Events.Queries;
 using EventsHub.Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -7,15 +8,26 @@ namespace EventsHub.Api.Controllers;
 public class EventsController : EventsHubBaseController
 {
     [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<Event>), StatusCodes.Status200OK)]
     public async Task<ActionResult<IReadOnlyList<Event>>> GetEventsAsync()
     {
         return await Mediator.Send(new GetEventList.Query());
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Event), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Event), StatusCodes.Status404NotFound)]
 
     public async Task<ActionResult<Event>> GetEventDetailAsync(string id)
     {
         return await Mediator.Send(new GetEventDetails.Query { Id = id });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<string>> CreateEventAsync(Event @event)
+    {
+        return await Mediator.Send(new CreateEvent.Command { Event = @event });
     }
 }
